6c51f0f1f2f87ba76bb02415ad58cbe5cb84fd83
Tej Dubey <your-github-email@example.com>
2026-07-30 00:58:58 +0530

    Fix model import case sensitivity

--- backend/routes/getUsers.js
+++ backend/routes/getUsers.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 
-const User=require("../models/User");
+const User=require("../models/user");
 const adminAuth=require("../middleware/adminAuth");
 
 router.get("/",adminAuth,async(req,res)=>{
